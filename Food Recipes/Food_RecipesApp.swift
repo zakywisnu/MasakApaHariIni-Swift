fc911d344fdaf3f100534a964bb7c19eebe4d807
Ahmad Zaky W <azakyw@gmail.com>
2021-04-23 08:56:00 +0700

    Initial Commit

---
+++ Food Recipes/Food_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_RecipesApp.swift
+//  Food Recipes
+//
+//  Created by Ahmad Zaky on 23/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
