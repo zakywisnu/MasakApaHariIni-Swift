4b1f6da9db43ed6e9e0c8b1f8f85fc88a4b53720
Ahmad Zaky W <azakyw@gmail.com>
2021-06-08 09:50:16 +0700

    Add error image placeholder

---
+++ Food Recipes/Core/Utils/Component/SearchRow.swift
@@ -0,0 +1,20 @@
+//
+//  SearchRow.swift
+//  Food Recipes
+//
+//  Created by Ahmad Zaky on 04/06/21.
+//
+
+import SwiftUI
+
+struct SearchRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchRow()
+    }
+}
